0bf30bac98cd501b69cd7eb48da779003f2b5df6
Rifat39 <rifat392000@gmail.com>
2023-05-19 00:18:55 +0600

    money

---
+++ script/money.js
@@ -0,0 +1,8 @@
+function show(str){
+    document.getElementById('sh2').style.display = 'none';
+    document.getElementById('sh1').style.display = 'block';
+}
+function show2(sign){
+    document.getElementById('sh2').style.display = 'block';
+    document.getElementById('sh1').style.display = 'none';
+}
