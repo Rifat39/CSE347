09f820626af4984cafd1fd4c68b9b3b4bb532e9f
Rifat39 <rifat392000@gmail.com>
2023-05-20 09:52:32 +0600

    some change

--- script/money.js
+++ script/money.js
@@ -6,3 +6,11 @@ function show2(sign){
     document.getElementById('sh2').style.display = 'block';
     document.getElementById('sh1').style.display = 'none';
 }
+function money(str){
+    document.getElementById('mo2').style.display = 'none';
+    document.getElementById('mo1').style.display = 'block';
+}
+function money2(sign){
+    document.getElementById('mo2').style.display = 'block';
+    document.getElementById('mo1').style.display = 'none';
+}
